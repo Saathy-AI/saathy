aee38f2182991aed74f185ad6f02016a2f6bf129
Jatin <aahuja.jatin@gmail.com>
2025-07-20 00:37:32 +0530

    fix ci

--- src/saathy/__init__.py
+++ src/saathy/__init__.py
@@ -1,7 +1,5 @@
 """Saathy - A FastAPI-based application."""
 
-from saathy.config import get_settings
-
 __version__ = "0.1.0"
 
-__all__ = ["__version__", "get_settings"]
+__all__ = ["__version__"]
